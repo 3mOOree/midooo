7e228b63e19f48fec1fe22ad598706eac53541cc
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-09-20 00:51:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -435,7 +435,7 @@ client.on('message', message => {
         let args = message.content.split(' ').slice(1).join(' ');
     if(message.content.split(' ')[0] == prefix + 'bc') {
         if (!args[1]) {
-    message.channel.send("**M$bc <message>**");
+    message.channel.send("**$bc <message>**");
     return;
     }
             message.guild.members.forEach(m => {
