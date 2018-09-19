337e3100fa21f7eb81a98243a70adcc34cfcff1e
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-09-20 00:21:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = '$$'
 
 
 
@@ -47,53 +46,6 @@ client.on('message', message=> {
     }
 });
 
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
 
 client.on('message', message => {
    if (message.content === "$$id") {
@@ -110,53 +62,6 @@ client.on('message', message => {
     }
 });
 
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
 
 client.on('message' , message => {;
   if(message.author.bot) return;
@@ -222,54 +127,6 @@ client.on('message' , message => {;
 }
  });
 
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-
 
 client.on("message", message => {
  
@@ -307,53 +164,7 @@ const config = require('./config.json');
 const allowedUsers = config.allowedUsers;
 const roles = config.roleToDisco;
  
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
+
  const cuttweet = [
      'كت تويت ‏| تخيّل لو أنك سترسم شيء وحيد فيصبح حقيقة، ماذا سترسم؟',
      'كت تويت | أكثر شيء يُسكِت الطفل برأيك؟',
@@ -397,36 +208,7 @@ const roles = config.roleToDisco;
     }
 });
 
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
+
 client.on('message', message => {
     var prefix = "$bc";
     
@@ -456,36 +238,7 @@ client.on('message', message => {
     });
     
 
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
+
 client.on('message', message => {
 	
 	if(message.author.bot) return;
@@ -787,29 +540,7 @@ client.on('message', message => {
 	}
 });
 
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
+
 
 client.on("message", async message => {
   if(message.author.bot) return;
