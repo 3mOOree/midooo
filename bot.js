e1a3b603f70cb26ef0837f1918318c97c1987e71
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-09-20 17:23:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1077,12 +1077,12 @@ if (command == "embed") {
 18
 19
 20
-\\\\\\\\\\\\\\\\\\\\\\\\ > M3a4x < \\\\\\\\\\\\\\\\\\\\\\\\
+
  client.on('message', message => {
         var  user = message.mentions.users.first() || message.author;
     if (message.content.startsWith("$avatar")) {
 message.channel.send(`This avatar For ${user} link : ${user.avatarURL}`);
 }
 });
-\\\\\\\\\\\\\\\\\\\\\\\\ > M3a4x < \\\\\\\\\\\\\\\\\\\\\\\\
+
 client.login(process.env.BOT_TOKEN);
