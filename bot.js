4a978b24ebfddfec597a33f764cb287127df00f4
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-09-20 13:57:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -941,30 +941,6 @@ var warn = message.guild.roles.find("name", "warn");
   }
   })
  
-client.on('message', message => {
-    if (message.content.startsWith(prefix + '$clear')) {
-      if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(`ماعندك هذا البرمشن[*MANAGE_MESSAGES*] `).catch(console.error);
-  message.delete()
-  if(!message.channel.guild) return;
-  let args = message.content.split(" ").slice(1);
- 
-  const messagecount = parseInt(args.join(' '));
- 
-  message.channel.fetchMessages({
- 
-  limit: messagecount
- 
-  }).then(messages => message.channel.bulkDelete(messages));
-  message.channel.sendMessage("", {embed: {
-    title: "``✏️✅ تــم مسح الشات ``",
-    color: 0x06DF00,
-    footer: {
-   
-    }
-    }}).then(msg => {msg.delete(3000)});
-  };
- 
-  });
 
 1
 2
