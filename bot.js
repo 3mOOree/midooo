296ed6ebba0e5177b158e85f631bc985c8036ead
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-09-21 22:37:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -240,7 +240,7 @@ client.on('message', message => {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "Dragon";
+    let copy = "MiDo_<3";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('```**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**```');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
     msg.react('✅')
@@ -838,7 +838,7 @@ client.on('message', message => {
         .setTitle('Click Here')
         .setURL(`${omar.avatarURL}`)
         .setImage(`${omar.avatarURL}`)
-        .setFooter('MiDo',client.user.avatarURL) 
+        .setFooter('MiDo_<3',client.user.avatarURL) 
       message.channel.sendEmbed(embed);
     }
 });
