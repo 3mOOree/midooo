dd8a107a28c9dcbefa2f14a0fda0c440eefd898e
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-09-24 04:02:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "$"
 const UserBlocked = new Set();
+const games = JSON.parse(fs.readFileSync("./games.json", "utf8"));
+
 
 
 
